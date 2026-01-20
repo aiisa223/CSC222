e858d391e2f4bd6e95fc7ef392d50e8a5f74cd2a
00ali <0.0aliimran0.01@gmail.com>
2026-01-20 14:14:03 -0500

    .

---
+++ src/module1/exc1.java
@@ -0,0 +1,11 @@
+package module1;
+
+public class exc1 {
+    static void main(String[] args) {
+        System.out.println("Ali Imran");
+        System.out.println("Computer Science");
+        System.out.println("I am taking 3 classes this semester");
+
+
+    }
+}
